3620d1d9ad18e53b94dba69e0b38ede73c72ecd3
alexvujisic <alexandar.vujisic@stud.fh-campuswien.ac.at>
2022-10-19 20:39:58 +0200

    Methode 4 Fertig

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -4,23 +4,23 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class App {
-    public static void oneMonthCalendar(int days, int weekday){
+    public static void oneMonthCalendar(int days, int weekday) {
         int zaehler = 1;
         int abstand = weekday - 1;
         int tag = weekday;
-        for(int i = 0; i < abstand; i++){
+        for (int i = 0; i < abstand; i++) {
             System.out.printf("   ");
         }
-        for(int i = 0; zaehler != (days + 1); i++){
-            for(; zaehler < 8 && tag < 8; tag++){
+        for (int i = 0; zaehler != (days + 1); i++) {
+            for (; zaehler < 8 && tag < 8; tag++) {
                 System.out.printf("%2d ", zaehler);
                 zaehler++;
             }
             //  System.out.print(" ");
-            for(; tag < 7; tag++){
+            for (; tag < 7; tag++) {
                 System.out.printf("%2d ", zaehler);
                 zaehler++;
-                if(zaehler == (days + 1)){
+                if (zaehler == (days + 1)) {
                     break;
                 }
             }
@@ -29,62 +29,85 @@ public class App {
         }
     }
 
-    public static long[] lcg(long seed){
+    public static long[] lcg(long seed) {
         long[] randNumbArray = new long[10];
-        final long m = (long) Math.pow(2, 31);
+        final double m = Math.pow(2, 31);
         final long a = 1103515245;
         final long c = 12345;
-        randNumbArray[0] = seed;
+        //seed = 12345;
+        //randNumbArray[0] = seed;
         //System.out.println("Zahl 1:  " + randNumbArray[0]);
-        for(int i = 0; i < randNumbArray.length; i++){
-            if(i != 9){
-                randNumbArray[i +1] = (((a * randNumbArray[i]) + c) % m);
-            }
-          //  System.out.println("Zahl " + i + "= " + randNumbArray[i] );
+        for (int i = 0; i < 10; i++) {
+            randNumbArray[i] = ((a * seed) + c) % (long) m;
+            seed = randNumbArray[i];
+            System.out.println("Zahl " + i + "= " + randNumbArray[i]);
         }
         return randNumbArray;
     }
 
-    public static void guessingGame(int numberToGuess){
+    public static void guessingGame(int numberToGuess) {
         Scanner scan = new Scanner(System.in);
         int versuch = 0;
         boolean flag = false;
-        for(int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             System.out.print("Guess number " + (i + 1) + ": ");
             versuch = scan.nextInt();
-            if((i < 9) && (versuch > numberToGuess)){
+            if ((i < 9) && (versuch > numberToGuess)) {
                 System.out.println("The number AI picked is lower than your guess.");
-            }
-            else if((i < 9) && (versuch < numberToGuess)){
+            } else if ((i < 9) && (versuch < numberToGuess)) {
                 System.out.println("The number AI picked is higher than your guess.");
-            }
-            else if(versuch == numberToGuess){
+            } else if (versuch == numberToGuess) {
                 System.out.println("You won wisenheimer!");
                 flag = true;
                 break;
             }
-          //  System.out.print("\n");
+            //  System.out.print("\n");
         }
-        if(flag == false){
+        if (flag == false) {
             System.out.println("You lost! Have you ever heard of divide & conquer?");
         }
     }
 
-    public static int randomNumberBetweenOneAndHundred(){
+    public static int randomNumberBetweenOneAndHundred() {
         Random random = new Random();
         return random.nextInt(99) + 1;
+    }
+
+    public static boolean swapArrays(int arrayA[], int arrayB[]) {
+        if (arrayA.length == arrayB.length) {
+            int[] arrayTemp = new int[arrayA.length];
+            for (int i = 0; i < arrayA.length; i++) {
+                arrayTemp[i] = arrayA[i];
+            }
+            for (int i = 0; i < arrayA.length; i++) {
+                arrayA[i] = arrayB[i];
+            }
+            for (int i = 0; i < arrayA.length; i++) {
+                arrayB[i] = arrayTemp[i];
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public static String camelCase(String eingabe[]){
+        String ergebnis = new String();
+
+        return ergebnis;
     }
 
     // Implement all methods as public static
 
     public static void main(String[] args) {
-        long seed = 12345;
+
+
+
      //   App.oneMonthCalendar(30, 2);
-      //  App.lcg(seed);
-       // for(int i = 0; i < lcg(seed).length; i++){
-         //   System.out.println("Array[" + i + "]=" + App.lcg(seed)[i]);
-      //  }
-        App.guessingGame(randomNumberBetweenOneAndHundred());
+       // App.lcg(12345);
+      //  App.guessingGame(randomNumberBetweenOneAndHundred());
+           // App.swapArrays(array1, array2);
+
+
 
         // Beispiel        String[] names = new Strinq[3];
         // test your method implementations here
